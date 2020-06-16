622f44f876efa7421a833b6e3e66ddada88cbd43
José Daniel Montoya Salazar <jd.montoya.s8@gmail.com>
2020-06-16 14:50:02 -0600

    Etapa IF completa

--- tb/IF_TB.sv
+++ tb/IF_TB.sv
@@ -3,22 +3,20 @@ module IF_TB();
 	parameter ARQ = 16;
 	parameter MEMORY_ADDR_SIZE = 6;
 	
-	logic clk, rst, pc_en;
-	logic[MEMORY_ADDR_SIZE-1:0] addr_in=6'b0, addr_out;
+	logic clk, rst, pc_en, mux_sel;
 	logic [ARQ-1:0] instr;
+	logic [MEMORY_ADDR_SIZE-1:0] branch_addr;
 
-	IF dut(clk, rst, pc_en, addr_in, addr_out, instr);
+	IF dut(clk, rst, pc_en, mux_sel, branch_addr, instr);
 
 initial begin
-	pc_en = 0; clk = 0; rst = 1;
+	pc_en = 1; clk = 0; rst = 1; mux_sel = 0; branch_addr=6'b1101;
 	
-	#50 rst = 1;
-	#50 rst = 0; pc_en = 1; addr_in<=addr_in+1;
-	#50 addr_in<=addr_in+1;
-	#50 addr_in<=addr_in+1;
-	#50 rst = 1;
-	#50 addr_in<=addr_in+1;
-	#50 addr_in<=addr_in+1;
+	#10 rst = 1;
+	#10 rst = 0;
+	#10 rst = 0;
+	#10 mux_sel = 1;
+	#10 mux_sel = 1;
 end
 
 always #5 clk = ~clk;
